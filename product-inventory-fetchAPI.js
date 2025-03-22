62a8708b22b89bcb011cb4ae70f4892dd0977624
saharhz <hosseinzadeh.sahar.h@gmail.com>
2025-03-22 15:01:53 +0100

    higher order function

--- product-inventory-fetchAPI.js
+++ product-inventory-fetchAPI.js
@@ -2,6 +2,22 @@ fetch("https://fakestoreapi.com/products/1")
   .then((response) => {
     return response.json();
   })
+  .then((products) => {
+    const totalProducts = products.length;
+    const totalPrice = products.reduce((sum, product) => sum + products.price);
+    const averagePrice = totalPrice / totalProducts;
+    const expensive = products.sort((max, product) => {
+      return product.price > max.price ? product : max;
+    });
+    const cheapest = products.sort((min, product) => {
+      return product.price > min.price ? product : min;
+    });
+
+    console.log(`Total number of Products: ${totalProducts}`);
+    console.log(`Average price: ${averagePrice}`);
+    console.log(`Most expensive: ${expensive}`);
+    console.log(`the cheapest: ${cheapest}`);
+  })
   .then((data) => {
     let { category, price } = data;
     console.log(category, price);
